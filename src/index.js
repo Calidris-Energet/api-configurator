10b7a6b5f1e039898b778c2f3eba8142a7e433ed
Yaroslav Mihalev <j.mihalev1018@yandex.ru>
2026-01-06 21:27:15 +0300

    feat: ansible

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const router = require('./routes/index')
 const errorHandler = require('./middleware/ErrorHandlingMiddleware')
 const path = require('path')
 
-const PORT = process.env.PORT || 5000
+const PORT = 5000
 
 const app = express()
 app.use(cors({
